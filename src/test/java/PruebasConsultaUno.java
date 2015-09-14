1b9949acbd7703c946c8fd1a2e143090f79b775a
Guillermo Alvarez <guillermo.alvarez@escuelaing.edu.co>
2015-09-13 22:36:22 -0500

    Pruebas Consulta 1 Mejoradas y terminadas

--- src/test/java/PruebasConsultaUno.java
+++ src/test/java/PruebasConsultaUno.java
@@ -70,7 +70,7 @@ public class PruebasConsultaUno {
     
     
     @Test
-    public void consultarPedidoPorCampesinos(){
+    public void unCampesinoPuedeProveerElProducto(){
         Transaction tx=session.beginTransaction();
         /****************************************************
             Almacenar los Productos de la prueba
@@ -139,7 +139,200 @@ public class PruebasConsultaUno {
             assertTrue("Se esperaba otro campesino",c.equals(c2));
         }
         assertTrue(true);
-        tx.commit();
+        tx.rollback();
     }     
      
+    @Test
+    public void ningunCampesinoVendeUnDeterminadoProducto(){
+        Transaction tx=session.beginTransaction();
+        /****************************************************
+            Almacenar los Productos de la prueba
+        *****************************************************/
+        Producto p1 = new Producto("Papa Criolla", 60, false);        
+        Producto p2 = new Producto("Papa Sabanera", 90, false);        
+        session.save(p1);session.save(p2);
+        /****************************************************
+            Almacenar las Ubicaciones de los campesinos
+        *****************************************************/        
+        Ubicacion u1 = new Ubicacion("Calle 123", "Cajica", "Cundinamarca", "20", "70");
+        Ubicacion u2 = new Ubicacion("Calle 234", "Chia", "Cundinamarca", "20", "80");
+        Ubicacion u3 = new Ubicacion("Casa de pobre y sin presencia", "Quibdo", "Choco", "40", "-80"); 
+        session.save(u1);session.save(u2);session.save(u3);
+        /****************************************************
+            Almacenar los campesinos
+        *****************************************************/               
+        Campesino c1 = new Campesino(1, u1, "Guillermo Leon", "Alvarez Salamanca", "1234567");
+        Campesino c2 = new Campesino(2, u2, "Andres Felipe", "Barrero Chacon", "1234567");      
+        Campesino c3 = new Campesino(3, u3, "Andres Mauricio", "Melo Torres", "1234567");                
+        session.save(c1);session.save(c2);session.save(c3);
+        /****************************************************
+            Almacenar los productos en venta del campesino 1
+        *****************************************************/            
+        Set <ProductoEnVenta> s1 = new HashSet<>();
+        ProductoEnVenta p1c1 = new ProductoEnVenta(c1, p1, "Muy buena calidad de papa criolla",
+                    new Date(2015-1900, 2, 5), (float)(60.5), 1000);    
+        session.save(p1c1);        
+        s1.add(p1c1);
+        c1 = (Campesino)session.load(Campesino.class, 1);
+        c1.setProductosEnVentas(s1);
+        session.update(c1);
+        /****************************************************
+            Almacenar los productos en venta del campesino 2
+        *****************************************************/         
+        Set<ProductoEnVenta> s2 = new HashSet<ProductoEnVenta>();
+        ProductoEnVenta p1c2 = new ProductoEnVenta(c2, p1, "La mejor papa criolla", 
+                new Date(2015-1900, 8, 8), (float)150.6, 1100);
+        session.save(p1c2);
+        s2.add(p1c2);
+        c2 = (Campesino)session.load(Campesino.class, 2);
+        c2.setProductosEnVentas(s2);
+        session.update(c2);        
+        /****************************************************
+            Almacenar los productos en venta del campesino 3
+        *****************************************************/   
+        Set <ProductoEnVenta> s3 = new HashSet<ProductoEnVenta>();
+        ProductoEnVenta p1c3 = new ProductoEnVenta(c3, p1, "La papa criolla de Zlatan", 
+                new Date(2015-1900, 8, 8), (float)300.50, 2500);
+        session.save(p1c3);
+        s3.add(p1c3);
+        c3 = (Campesino)session.load(Campesino.class, 3);
+        c3.setProductosEnVentas(s3);
+        session.update(c3);
+        /****************************************************
+            Se realizara la consulta de los campesinos que pueden proveer un
+            determinado producto y se proseguira con la prueba
+        *****************************************************/          
+        List <Campesino> answer = PersistenceFacade.campesinosPorProducto(session, p2, 20);
+        assertTrue("Se esperaba un conjunto vacio",answer.size()==0);                
+        tx.rollback();
+    }     
+    
+    @Test
+    public void ningunCampesinoTieneElProductoEnBuenEstado(){
+        Transaction tx=session.beginTransaction();
+        /****************************************************
+            Almacenar los Productos de la prueba
+        *****************************************************/
+        Producto p1 = new Producto("Papa Criolla", 60, false);                
+        session.save(p1);
+        /****************************************************
+            Almacenar las Ubicaciones de los campesinos
+        *****************************************************/        
+        Ubicacion u1 = new Ubicacion("Calle 123", "Cajica", "Cundinamarca", "20", "70");
+        Ubicacion u2 = new Ubicacion("Calle 234", "Chia", "Cundinamarca", "20", "80");
+        Ubicacion u3 = new Ubicacion("Casa de pobre y sin presencia", "Quibdo", "Choco", "40", "-80"); 
+        session.save(u1);session.save(u2);session.save(u3);
+        /****************************************************
+            Almacenar los campesinos
+        *****************************************************/               
+        Campesino c1 = new Campesino(1, u1, "Guillermo Leon", "Alvarez Salamanca", "1234567");
+        Campesino c2 = new Campesino(2, u2, "Andres Felipe", "Barrero Chacon", "1234567");      
+        Campesino c3 = new Campesino(3, u3, "Andres Mauricio", "Melo Torres", "1234567");                
+        session.save(c1);session.save(c2);session.save(c3);
+        /****************************************************
+            Almacenar los productos en venta del campesino 1
+        *****************************************************/            
+        Set <ProductoEnVenta> s1 = new HashSet<>();
+        ProductoEnVenta p1c1 = new ProductoEnVenta(c1, p1, "Muy buena calidad de papa criolla",
+                    new Date(2015-1900, 1, 12), (float)(60.5), 1000);    
+        session.save(p1c1);        
+        s1.add(p1c1);
+        c1 = (Campesino)session.load(Campesino.class, 1);
+        c1.setProductosEnVentas(s1);
+        session.update(c1);
+        /****************************************************
+            Almacenar los productos en venta del campesino 2
+        *****************************************************/         
+        Set<ProductoEnVenta> s2 = new HashSet<ProductoEnVenta>();
+        ProductoEnVenta p1c2 = new ProductoEnVenta(c2, p1, "La mejor papa criolla", 
+                new Date(2015-1900, 3, 8), (float)150.6, 1100);
+        session.save(p1c2);
+        s2.add(p1c2);
+        c2 = (Campesino)session.load(Campesino.class, 2);
+        c2.setProductosEnVentas(s2);
+        session.update(c2);        
+        /****************************************************
+            Almacenar los productos en venta del campesino 3
+        *****************************************************/   
+        Set <ProductoEnVenta> s3 = new HashSet<ProductoEnVenta>();
+        ProductoEnVenta p1c3 = new ProductoEnVenta(c3, p1, "La papa criolla de Zlatan", 
+                new Date(2015-1900, 4, 20), (float)300.50, 2500);
+        session.save(p1c3);
+        s3.add(p1c3);
+        c3 = (Campesino)session.load(Campesino.class, 3);
+        c3.setProductosEnVentas(s3);
+        session.update(c3);
+        /****************************************************
+            Se realizara la consulta de los campesinos que pueden proveer un
+            determinado producto y se proseguira con la prueba
+        *****************************************************/          
+        List <Campesino> answer = PersistenceFacade.campesinosPorProducto(session, p1, 10);
+        assertTrue("Se esperaba un conjunto vacio",answer.size()==0);                
+        tx.rollback();
+    }     
+    
+    @Test
+    public void ningunCampesinoTieneLaCantidadDeProductoNecesaria(){
+        Transaction tx=session.beginTransaction();
+        /****************************************************
+            Almacenar los Productos de la prueba
+        *****************************************************/
+        Producto p1 = new Producto("Papa Criolla", 60, false);                
+        session.save(p1);
+        /****************************************************
+            Almacenar las Ubicaciones de los campesinos
+        *****************************************************/        
+        Ubicacion u1 = new Ubicacion("Calle 123", "Cajica", "Cundinamarca", "20", "70");
+        Ubicacion u2 = new Ubicacion("Calle 234", "Chia", "Cundinamarca", "20", "80");
+        Ubicacion u3 = new Ubicacion("Casa de pobre y sin presencia", "Quibdo", "Choco", "40", "-80"); 
+        session.save(u1);session.save(u2);session.save(u3);
+        /****************************************************
+            Almacenar los campesinos
+        *****************************************************/               
+        Campesino c1 = new Campesino(1, u1, "Guillermo Leon", "Alvarez Salamanca", "1234567");
+        Campesino c2 = new Campesino(2, u2, "Andres Felipe", "Barrero Chacon", "1234567");      
+        Campesino c3 = new Campesino(3, u3, "Andres Mauricio", "Melo Torres", "1234567");                
+        session.save(c1);session.save(c2);session.save(c3);
+        /****************************************************
+            Almacenar los productos en venta del campesino 1
+        *****************************************************/            
+        Set <ProductoEnVenta> s1 = new HashSet<>();
+        ProductoEnVenta p1c1 = new ProductoEnVenta(c1, p1, "Muy buena calidad de papa criolla",
+                    new Date(2015-1900, 1, 12), (float)(60.5), 1000);    
+        session.save(p1c1);        
+        s1.add(p1c1);
+        c1 = (Campesino)session.load(Campesino.class, 1);
+        c1.setProductosEnVentas(s1);
+        session.update(c1);
+        /****************************************************
+            Almacenar los productos en venta del campesino 2
+        *****************************************************/         
+        Set<ProductoEnVenta> s2 = new HashSet<ProductoEnVenta>();
+        ProductoEnVenta p1c2 = new ProductoEnVenta(c2, p1, "La mejor papa criolla", 
+                new Date(2015-1900, 3, 8), (float)150.6, 1100);
+        session.save(p1c2);
+        s2.add(p1c2);
+        c2 = (Campesino)session.load(Campesino.class, 2);
+        c2.setProductosEnVentas(s2);
+        session.update(c2);        
+        /****************************************************
+            Almacenar los productos en venta del campesino 3
+        *****************************************************/   
+        Set <ProductoEnVenta> s3 = new HashSet<ProductoEnVenta>();
+        ProductoEnVenta p1c3 = new ProductoEnVenta(c3, p1, "La papa criolla de Zlatan", 
+                new Date(2015-1900, 4, 20), (float)300.50, 2500);
+        session.save(p1c3);
+        s3.add(p1c3);
+        c3 = (Campesino)session.load(Campesino.class, 3);
+        c3.setProductosEnVentas(s3);
+        session.update(c3);
+        /****************************************************
+            Se realizara la consulta de los campesinos que pueden proveer un
+            determinado producto y se proseguira con la prueba
+        *****************************************************/          
+        List <Campesino> answer = PersistenceFacade.campesinosPorProducto(session, p1, 1000);
+        assertTrue("Se esperaba un conjunto vacio",answer.size()==0);                
+        tx.rollback();
+    }     
+    
 }
